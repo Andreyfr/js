100245fd8d623ebbc470963fd7b750994a200d37
Andreyfr <andrey.levko90@gmail.com>
2019-04-19 15:48:31 +0300

    Update connection_mysql.js

--- connection_mysql.js
+++ connection_mysql.js
@@ -33,3 +33,38 @@ or
 or 
            echo $MYSECRETPASSWD
 */
+/*
+You can use the script below to use multiple samples.
+the only difference is ( "if (error) throw error" and "if (error){cosnole.log(error)}")
+*/
+var mysql = require('mysql')
+var cors = require('cors')
+var userName = process.env.USERNAME
+var passwd = process.env.MYSECRETPASSWD
+var dbName = process.env.DB
+
+var connection = mysql.createConnection({
+       host: 'localhost',
+       user: userName,
+       password: passwd,
+       database: dbName 
+})
+
+connection.connect()
+
+connection.query(
+       'select * from test_an_user',
+       function (error, result){
+              if (error){cosnole.log(error)}
+              console.table(result)
+       }
+)
+connection.query(
+       'select costs from test_an_user',
+       function (errors, result){
+              if (errors) {console.log(errors)}
+              console.table(result)
+       }
+)
+
+connection.end()
